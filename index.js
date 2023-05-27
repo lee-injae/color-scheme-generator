2e68e1e26c71bf8c6821570a797147073a369de9
Injae Lee <ilee.injae@gmail.com>
2023-05-27 02:16:13 -0400

    changing input color function added

--- index.js
+++ index.js
@@ -1,56 +1,84 @@
 const url = "https://www.thecolorapi.com"
 const schemeEndpoint = "/scheme"
 const colorEndpoint = "color"
-const selectElement = document.getElementById("colors");
+const selectEl = document.getElementById("colors");
 const getColorBtn = document.getElementById("get-color-btn")
+const colorInputEl = document.getElementById("start-color")
+
 const schemesArr = ["monochrome", "monochrome-dark", "monochrome-light", "analogic", "complement", "analogic-complement", "triad", "quad"]
 
-const startColorHex = "800080"
+let startColorHex = "F55A5A"
 let selectedSchemeName = ""
 
-function distributeSchemes(arr){
-    let htmlOption = ""
-    for (scheme of schemesArr){
-        htmlOption += `
-            <option id=${scheme} value="${scheme}">${scheme}</option>
-        `
-    }
-    document.getElementById("colors").innerHTML = htmlOption
-}
 
-distributeSchemes(schemesArr)
+colorInputEl.addEventListener("change", e => {
+    console.log("change")
+    startColorHex = e.target.value.slice(1)
+})
 
-selectElement.addEventListener('change', function(e) {
+selectEl.addEventListener('change', e => {
     selectedSchemeName = e.target.value
-    console.log("selectedName: ", selectedSchemeName)
 })
 
 getColorBtn.addEventListener("click", e => { 
-    handleGetColorScheme(e, selectedSchemeName)})
+    handleGetColorScheme(e, selectedSchemeName)
+})
 
 function handleGetColorScheme(e, string){
     e.preventDefault()
-    fetch(`${url}${schemeEndpoint}?hex=${startColorHex}&mode=${string}`)
-        .then(res => res.json())
-        .then(data => {
-            // data.colors is an array of five color objects with hexcode, rgb values nested as objects
-            renderColors(data.colors)
-        })
+    getColors(string)
 }
 
-function renderColors(arr){
+function getColors(string){
+    fetch(`${url}${schemeEndpoint}?hex=${startColorHex}&mode=${string}`)
+    .then(res => res.json())
+    .then(data => {
+        // data.colors is an array of five color objects with hexcode, rgb values nested as objects
+        renderColorsHtml(data.colors)
+    })
+}
+
+function renderColorsHtml(arr){
     let html = ""
+    let footerDivs = ""
     arr.forEach( colorObj => {
+
         html += `
-            <div class="color-bar" 
-                style="background: ${colorObj.hex.value};">
-            </div>
+        <div class="color-bar" 
+            style="background: ${colorObj.hex.value};">
+        </div>
+        `
+
+        footerDivs += `
+            <div id="footer-${colorObj.hex.value}">${colorObj.hex.value}</div>
         `
     });
     document.getElementById("colors-container").innerHTML = html
+    document.getElementById("footer").innerHTML = footerDivs
+
 }
 
-
+function distributeSchemeOptions(arr){
+    let selectOptions = ""
+    
+    for (scheme of schemesArr){
+        selectOptions += `
+            <option id=${scheme} value="${scheme}">${scheme}</option>
+        `
+        
+    }
+    document.getElementById("colors").innerHTML = selectOptions
+}
+        
+function assignStartColor(string){
+    document.getElementById("start-color").value = "#" + string
+}
+    
+    
+    
+assignStartColor(startColorHex)
+getColors("monochrome")
+distributeSchemeOptions(schemesArr)
 
 
 
